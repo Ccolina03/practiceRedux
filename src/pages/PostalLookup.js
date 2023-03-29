35442ba90f6f4cf596cc1681e8ac16475b234df7
Ccolina03 <colinaperu2003@gmail.com>
2023-03-28 22:11:24 -0600

    Postal Lookup Commit

---
+++ src/pages/PostalLookup.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostalLookup = () => {
+    return (
+        <div> 
+            <p>Look for Postal lookup</p>
+        </div>
+    )
+}
+
+export default PostalLookup;
